2902666d3532cc60f9843b0b657b199baad0f679
KrystianKasp98 <krystiankasprzak98byd@gmail.com>
2021-11-24 19:55:39 +0100

    Finished V1

--- scripts/Game.esm.js
+++ scripts/Game.esm.js
@@ -1,6 +1,6 @@
 import { UI } from './UI.esm.js';
 import { Field } from './Field.esm.js';
-import { BOARD_GAME_SELECTOR,FIELD_SELECTOR,MODAL_BUTTON_SELECTOR,MODAL_WRAPPER_SELECTOR, BOARD_ROWS,BOARD_COLS, VARIABLE_ROWS_CSS, VARIABLE_COLS_CSS, BACKGROUND_IMAGE_O_CSS, BACKGROUND_IMAGE_X_CSS, MARK_FIELD_O, MARK_FIELD_X, MOVE_SELECTOR, PLAYER_1_TURN_TEXT, PLAYER_2_TURN_TEXT, VISIBILITY_CLASS_CSS,MODAL_RESULT_TEXT_SELECTOR,DRAW_TEXT, BACKGROUND_IMAGE_EMPTY_CSS} from './Statements.esm.js';
+import { BOARD_GAME_SELECTOR,FIELD_SELECTOR,MODAL_BUTTON_SELECTOR,MODAL_WRAPPER_SELECTOR, BOARD_ROWS,BOARD_COLS, VARIABLE_ROWS_CSS, VARIABLE_COLS_CSS, BACKGROUND_IMAGE_O_CSS, BACKGROUND_IMAGE_X_CSS, MARK_FIELD_O, MARK_FIELD_X, MOVE_SELECTOR, PLAYER_1_TURN_TEXT, PLAYER_2_TURN_TEXT, VISIBILITY_CLASS_CSS,MODAL_RESULT_TEXT_SELECTOR,DRAW_TEXT, BACKGROUND_IMAGE_EMPTY_CSS,PLAYER_1_WIN_TEXT,PLAYER_2_WIN_TEXT} from './Statements.esm.js';
 
 export class Game extends UI{
   constructor() {
@@ -8,13 +8,14 @@ export class Game extends UI{
     super();
     this.board = this.getElement(BOARD_GAME_SELECTOR);
     this.button = this.getElement(MODAL_BUTTON_SELECTOR);
-    this.rows = BOARD_ROWS;
+    this.rows = BOARD_ROWS;//niestety za duzo bledow popelnilem ponizej
     this.cols = BOARD_COLS;
 
     //properties to restart
     this.arrayOfFields = [];
     this.gameMoves = 0;
     this.endGameMoves = this.rows * this.cols;
+    this.isGameEnded = false;
 
     this.addEventListeners();
   }
@@ -37,19 +38,31 @@ export class Game extends UI{
   resetGame() {
     this.gameMoves = 0;
     this.endGameMoves = this.rows * this.cols;
+    this.isGameEnded = false;
     this.startGame();
   }
   generateFields() {
     //reseting
     this.arrayOfFields = [];
-
-    //creating 2d array of fields
-    for (let row = 0; row < this.rows; row++){
-      this.arrayOfFields[row] = [];
-      for (let col = 0; col < this.cols; col++){
-        this.arrayOfFields[row].push(new Field(col, row));
+    //when cols > rows
+    if (this.cols > this.rows) {
+      for (let col = 0; col < this.cols; col++) {
+        this.arrayOfFields[col] = [];
+        for (let row = 0; row < this.rows; row++) {
+          this.arrayOfFields[col].push(new Field(row, col));
+        }
       }
     }
+    else {
+      //creating 2d array of fields
+      for (let row = 0; row < this.rows; row++) {
+        this.arrayOfFields[row] = [];
+        for (let col = 0; col < this.cols; col++) {
+          this.arrayOfFields[row].push(new Field(col, row));
+        }
+      }
+    }
+    
   }
   renderBoard() {
     this.arrayOfFields.flat().forEach(field => {
@@ -84,6 +97,7 @@ export class Game extends UI{
   }
   //left click event
   clickField = (e) => {
+    if (this.isGameEnded)  return;
     const field = this.findField(e);
     //protection against unexpected increments
     if (field.checkIsFieldMarked())
@@ -99,8 +113,9 @@ export class Game extends UI{
     else if (this.gameMoves % 2 == 1) {
       this.renderField(field, MARK_FIELD_O, BACKGROUND_IMAGE_O_CSS, PLAYER_1_TURN_TEXT);
     }
-    this.checkFieldsAround(field, this.arrayOfFields);
     this.gameMoves++;
+    this.checkFieldsAround(field, this.arrayOfFields);
+    
     
     
     this.checkDraw();
@@ -123,63 +138,75 @@ export class Game extends UI{
     //skos TL ->BR
     let up_2_left_2 = colIndex - 2 >= 0 && rowIndex - 2 >= 0 ? fields[colIndex - 2][rowIndex - 2] : false;
     let up_1_left_1 = colIndex - 1 >= 0 && rowIndex - 1 >= 0 ? fields[colIndex - 1][rowIndex - 1] : false;
-    let down_1_right_1 = colIndex + 1 < this.cols && rowIndex + 1 < this.rows < this.cols ? fields[colIndex + 1][rowIndex + 1] : false;
+    let down_1_right_1 = colIndex + 1 < this.cols && rowIndex + 1 < this.rows ? fields[colIndex + 1][rowIndex + 1] : false;
     let down_2_right_2 = colIndex + 2 < this.cols && rowIndex + 2 < this.rows < this.cols ? fields[colIndex + 2][rowIndex + 2] : false;
 
     //skos BL -> TR
-    
-
+    let down_2_left_2 = colIndex + 2 < this.cols && rowIndex - 2 >= 0 ? fields[colIndex + 2][rowIndex - 2] : false;
+    let down_1_left_1 = colIndex + 1 < this.cols && rowIndex - 1 >= 0 ? fields[colIndex + 1][rowIndex - 1] : false;
+    let up_1_right_1 = colIndex - 1 >= 0 && rowIndex + 1 < this.rows ? fields[colIndex - 1][rowIndex + 1] : false;
+    let up_2_right_2 = colIndex - 2 >= 0 && rowIndex + 2 < this.rows ? fields[colIndex - 2][rowIndex + 2] : false;
 
     //down_1 down_2
     if (down_2 !== false && down_1 !== false) this.checkResultXorO(field, down_1, down_2);
     //down_1 up_1
-    else if (up_1 !== false && down_1 !== false) this.checkResultXorO(field, down_1, up_1);
+    if (up_1 !== false && down_1 !== false) this.checkResultXorO(field, down_1, up_1);
     //up_1 up_2
-    else if (up_1 !== false && up_2 !== false) this.checkResultXorO(field, up_1, up_2);
+    if (up_1 !== false && up_2 !== false) this.checkResultXorO(field, up_1, up_2);
   
     //left 1 left 2
     if (left_1 !== false && left_2 !== false) this.checkResultXorO(field, left_1, left_2);
     //left 1 right 1
-    else if (left_1 !== false && right_1 !== false) this.checkResultXorO(field, left_1, right_1);
+    if (left_1 !== false && right_1 !== false) this.checkResultXorO(field, left_1, right_1);
     //right 1 right 2
-    else if (right_1 !== false && right_2 !== false) this.checkResultXorO(field, right_1, right_2);
+    if (right_1 !== false && right_2 !== false) this.checkResultXorO(field, right_1, right_2);
 
+    //skos TL ->BR
+    if (up_2_left_2 !== false && up_1_left_1 !== false) this.checkResultXorO(field, up_2_left_2, up_1_left_1);
+    if (up_1_left_1 !== false && down_1_right_1 !== false) this.checkResultXorO(field, up_1_left_1, down_1_right_1);
+    if (down_1_right_1 !== false && down_2_right_2 !== false) this.checkResultXorO(field, down_1_right_1, down_2_right_2);
 
-
-    //wygrana w iksach
-    // if (rowIndex + 2 <= this.rows) {
-    //   const up2 = fields[colIndex][rowIndex + 2];
-    // } 
-    // if (rowIndex + 1 <= this.rows)
-    // {
-    //   const up1 = fields[colIndex][rowIndex + 1];
-    // }
+    //skos BL -> TR
+    if (down_2_left_2 !== false && down_1_left_1 !== false) this.checkResultXorO(field, down_2_left_2, down_1_left_1);
+    if (down_1_left_1 !== false && up_1_right_1 !== false) this.checkResultXorO(field, down_1_left_1, up_1_right_1);
+    if (up_2_right_2 !== false && up_1_right_1 !== false) this.checkResultXorO(field, up_2_right_2, up_1_right_1);
     
   }
   //rostrzyga czy sprawdzac x czy o i zwraca wynik
   checkResultXorO(field_1, field_2, field_3) {
-    if (this.gameMoves % 2 == 0) {
+    if (this.gameMoves % 2 == 1) {
       if (field_1.isMarkedAsX && field_2.isMarkedAsX && field_3.isMarkedAsX) {
-        alert('player 1 win');
+        setTimeout(() => {
+          this.toogleModal(PLAYER_1_WIN_TEXT);
+          this.isGameEnded = true;
+          console.log(field_1.selector, field_2.selector, field_3.selector);
+        },1500)
+        
       }
-    } else {
+    } else if (this.gameMoves % 2 == 0) {
       if (field_1.isMarkedAsO && field_2.isMarkedAsO && field_3.isMarkedAsO) {
-        alert('player 2 win');
+        setTimeout(() => {
+          this.toogleModal(PLAYER_2_WIN_TEXT);
+          this.isGameEnded = true;
+          console.log(field_1.selector, field_2.selector, field_3.selector);
+        },1500) 
       }
     }
-    // console.log(field_1, field_2, field_3);
   }
 
   //checking result
   checkDraw() {
     if (this.gameMoves === this.endGameMoves) {
-      this.toogleModal();//makes modal visible
+      setTimeout(() => {
+        this.toogleModal(DRAW_TEXT); //makes modal visible
+        this.isGameEnded = true;
+      },1500)
     }
   }
   //change modal visibility
-  toogleModal() {
-    this.getElement(MODAL_WRAPPER_SELECTOR).classList.toggle(VISIBILITY_CLASS_CSS);
-    this.updateText(MODAL_RESULT_TEXT_SELECTOR, DRAW_TEXT);
+  toogleModal(text) {
+      this.getElement(MODAL_WRAPPER_SELECTOR).classList.toggle(VISIBILITY_CLASS_CSS);
+      this.updateText(MODAL_RESULT_TEXT_SELECTOR, text);
   }
 }
 
--- scripts/Statements.esm.js
+++ scripts/Statements.esm.js
@@ -8,7 +8,7 @@ export const MODAL_RESULT_TEXT_SELECTOR = '[data-result]';
 
 //game size
 export const BOARD_ROWS = 3;
-export const BOARD_COLS = 3;
+export const BOARD_COLS = 6;
 
 // css
 export const VARIABLE_ROWS_CSS = '--field-rows';
@@ -25,3 +25,5 @@ export const MARK_FIELD_O = 'O';
 export const PLAYER_1_TURN_TEXT = `1 player's move`;
 export const PLAYER_2_TURN_TEXT = `2 player's move`;
 export const DRAW_TEXT = `draw, play again`;
+export const PLAYER_1_WIN_TEXT = `eXcellent, game player 1 won`;
+export const PLAYER_2_WIN_TEXT = `awesOme, game player 2 won`;
